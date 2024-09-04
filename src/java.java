7a10a48689e568571f28f8dab38b8f417fd33d33
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-09-04 13:58:37 -0400

    yoo

--- src/java.java
+++ src/java.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class java {
+public class java {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
+
+    public static boolean isPalindrome(int num){
+        number = Math.abs(num);
+
+        int numberClone = num;
+        int reverse = 0;
+
+        while (number > 0){
+            int lastDigit = number % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            num /= 10;
+        }
+
+
+        lastDigit = num % 10{
+            reverse *= 10;
+            reverse += lastDigit;
+            num /= 10;
+        return reverse == numberClone;
+
+
+
+    }
+
 }
+
+
+
